fcc1739dbdda0c56121cb2962084e45b7b0e36f5
62630619 <sumanadasrh@gmail.com>
2025-03-02 18:44:20 +0530

    backend

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
-const con=mongoose.connect('mongodb://localhost:27017/PRactice',{useNewUrlParser:true,useUnifiedTopology:true})
+const con=mongoose.connect('mongodb+srv://websitesudharo:1ox6G2TydUsezmiy@contactcrud.vlqhn.mongodb.net/?retryWrites=true&w=majority&appName=contactCrud',{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
     console.log("connected to database");
 })
 .catch((err)=>{
     console.log(err);
 });
+
+
+
+
+// 1ox6G2TydUsezmiy
